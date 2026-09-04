2fb5cd7a70cb2a91f6aa7061e55cc7f42ab93a23
BarnabasHtoo <barnabasthadohtoo@gmail.com>
2026-09-04 09:26:14 -0500

    Fix indentation in main function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {  cout << "Welcome to the Silly Simply Math Tutor V1" << endl ;
+int main() {  
+    cout << "Welcome to the Silly Simply Math Tutor V1" << endl ;
     return 0;
-
 }               
