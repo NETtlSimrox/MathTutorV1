91960cdc08e3585e42cf487727ff69fa62b516bb
BarnabasHtoo <barnabasthadohtoo@gmial.com>
2026-09-09 13:11:26 -0500

    Added some animation at the beginning and the end.

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 int main() {
     string userName = "unknown";
+    string statement_i;
     int firstNum = 2;
     int secondNum = 3;
-    int statement1;
-    int statement2;
-    int answer = 0;
+    int userAns;
+    int answer = 5;
+
 
     cout << " __  __       _   _       _____      _ " << endl;
     cout << "|  \\/  | __ _| |_| |__   |_   _|   _| |_ ___  _ __ " << endl;
@@ -24,12 +25,12 @@ int main() {
     cout << "Welcome " << userName << "!" << endl;
 
     cout << "Are you ready to guess the answer?" << endl;
-    cin >> statement1;
+    cin >> statement_i;
 
     cout << "Here we go!" << endl;
     cout << firstNum << " + " << secondNum << " = " << "?" << endl;
     cout << "Please type the answer below!" << endl;
-    cin >> statement2;
+    cin >> userAns;
 
     cout << "We will tell you the correct answer on our MathTutor Vers. II" << endl;
     cout << "Stay Turn!" << endl;
